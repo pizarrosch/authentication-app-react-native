47c2a1d2bfd7c631fb9b947aabbf441dc47d9547
pizarrosch <bayenrshc89@gmail.com>
2024-07-06 22:59:26 +0200

    Create an http (POST) request to sign up a new user

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,22 @@
 import AuthContent from '../components/Auth/AuthContent';
+import {createUser} from "../ui/auth";
+import {useState} from "react";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function signupHandler({email, password}) {
+    setIsAuthenticating(true);
+    await createUser(email, password);
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message='User being created...'/>
+  }
+
+  return <AuthContent onAuthenticate={signupHandler}/>;
 }
 
 export default SignupScreen;
